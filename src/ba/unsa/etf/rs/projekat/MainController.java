1727c77bce52bb4a17d17a4f83329784b86e00b5
Emina Palalic <epalalic1@etf.unsa.ba>
2021-08-07 11:26:48 +0200

    Izgled aplikacije ukoliko je korisnik srednjoskolac

--- src/ba/unsa/etf/rs/projekat/MainController.java
+++ src/ba/unsa/etf/rs/projekat/MainController.java
@@ -3,9 +3,15 @@ package ba.unsa.etf.rs.projekat;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MainController {
     @FXML
@@ -18,7 +24,12 @@ public class MainController {
     }
 
     public void srednjoskolac(ActionEvent actionEvent) throws IOException {
-        BorderPane pane1 = FXMLLoader.load(getClass().getResource("/fxml/srednjoskolac.fxml"));
-        pane.getChildren().setAll(pane1);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/srednjoskolac.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage myStage = new Stage();
+        myStage.setTitle("Biljeske s predavanja");
+        myStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.setFullScreen(true);
+        myStage.show();
     }
 }
